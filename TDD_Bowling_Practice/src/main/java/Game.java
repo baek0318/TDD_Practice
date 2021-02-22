27a7a50c6a748b0847527781c29fc45c77cc1b7a
peachberry <peachberry0318@gmail.com>
2021-02-22 11:41:17 +0900

    Game 객체 완료

--- TDD_Bowling_Practice/src/main/java/Game.java
+++ TDD_Bowling_Practice/src/main/java/Game.java
@@ -1,7 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Game {
 
-    public int start() {
-        return 300;
+    List<Frame> frames;
+
+    public Game() {
+        frames = new ArrayList<>();
+        for(int i = 0; i < 12; i++) {
+            frames.add(new Frame());
+        }
+    }
+
+    public int start(int first, int second) {
+        for(int i = 0; i < frames.size(); i++){
+            frames.get(i).roll(first, second);
+            if(i >= 1) {
+                if(frames.get(i-1).getIsSpare()) {
+                    Calculator.spareCalc(frames.get(i-1), frames.get(i));
+                }
+            }
+            if(i >= 2) {
+                if(frames.get(i-2).getIsStrike()) {
+                    Calculator.strikeCalc(frames.get(i-2), frames.get(i-1), frames.get(i));
+                }
+            }
+        }
+        return end();
+    }
+
+    private int end() {
+        return Calculator.totalCalc(frames);
     }
 }
--- TDD_Bowling_Practice/src/test/java/GameTest.java
+++ TDD_Bowling_Practice/src/test/java/GameTest.java
@@ -4,21 +4,35 @@ import org.junit.jupiter.api.Test;
 
 public class GameTest {
 
+    //Todo 4. 마지막 프레임 스트라이크인 경우 +2 프레임 추가제공
+    //Todo 5. 마지막 프레임 스페어인 경우 +1 프레임 추가제공
+
     //10번의 게임을 진행할때 10번 모두 스트라이크라면 300점을 반환해야한다
     //한 게임당 30점의 점수를 내야한다
-
     @Test
-    @DisplayName("모두 스트라이크라면 300점을 반환해야한다")
-    void testGame() {
+    @DisplayName("모두 스트라이크라면 300점을 반환")
+    void testStrikeGame() {
         Game game = new Game();
-        int result = game.start();
+        int result = game.start(10, 0);
 
         Assertions.assertEquals(300, result);
     }
 
     @Test
-    @DisplayName("스페어시에 점수 계산")
-    void testSpare() {
+    @DisplayName("모두 5,5 스페어라면 150점 반환")
+    void test55SpareGame() {
+        Game game = new Game();
+        int result = game.start(5,5);
 
+        Assertions.assertEquals(150, result);
+    }
+
+    @Test
+    @DisplayName("모두 스페어와 스트라이크가 아닌 경우")
+    void testNotSpareAndStrike() {
+        Game game = new Game();
+        int result = game.start(3, 4);
+
+        Assertions.assertEquals(70, result);
     }
 }
